a2c473486504d4faabf81a325122d6a62a6980f3
ashvini bhosale <ashumbhosale@gmail.com>
2021-07-20 19:14:59 +0530

    First commit to gem

---
+++ lib/apis.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require "rest-client"
+require_relative "apis/version"
+
+module Apis
+  class Responses
+  	def self.awesome?
+      puts "Hello world!!!!!!!!!!"
+      response = RestClient.post 'https://api.nowcerts.com/api/token', {grant_type:'password', username:'api@api.api', password:'123456Qw', client_id:'ngAuthApp'}, {content_type: 'text/plain'}
+      puts "#{response}"
+    end
+  end
+  # Your code goes here...
+end
