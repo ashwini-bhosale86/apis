87947fa57a6a19441a9482d99ea0c954e6129781
ashvini bhosale <ashumbhosale@gmail.com>
2021-07-22 12:30:01 +0530

    creds changes

--- lib/apis.rb
+++ lib/apis.rb
@@ -6,7 +6,7 @@ module Apis
   class Responses
   	def self.awesome?
       puts "Hello world!!!!!!!!!!"
-      response = RestClient.post 'https://api.nowcerts.com/api/token', {grant_type:'password', username:'api@api.api', password:'123456Qw', client_id:'ngAuthApp'}, {content_type: 'text/plain'}
+      response = RestClient.post 'https://api.nowcerts.com/api/token', {grant_type:'password', username:'api', password:'123456Qw', client_id:'ngAuthApp'}, {content_type: 'text/plain'}
       puts "#{response}"
     end
   end
