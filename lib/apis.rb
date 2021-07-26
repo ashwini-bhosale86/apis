314d8acc01a97b41e73f5fc2b1f95e380c1c074a
ashish <achougule@opengov.com>
2021-07-26 15:38:49 +0530

    Refactored code

--- lib/apis.rb
+++ lib/apis.rb
@@ -6,32 +6,21 @@ require_relative 'apis/nowcerts'
 
 module Apis
   class Responses
-  	def self.awesome?
+    def self.awesome?
       response = RestClient.post 'https://api.nowcerts.com/api/token', {grant_type:'password', username:'api', password:'123456Qw', client_id:'ngAuthApp'}, {content_type: 'text/plain'}
     end
 
     def self.authorize(type, url, header)
-      resp = request(type, url, header)
-      response(resp)
-      # @r
+      request(type, url, header)
     end
 
     def self.request(type, url, header, authorization = nil)
-      RestClient::Request.execute(method: type.to_sym, url: url, payload: header, headers: {content_type: 'text/plain', authorization: authorization})
+      resp = RestClient::Request.execute(method: type.to_sym, url: url, payload: header, headers: {content_type: 'text/plain', authorization: authorization})
+      response(resp)
     end
 
     def self.response(resp)
-      response = JSON.parse(resp)
+      JSON.parse(resp)
     end
-
-    # def self.get_agency_list(type, url, header, access_token) 
-    #   # Apis::Responses.request('get',
-    #   # 'https://api.nowcerts.com/api/PolicyDetailList()?$count=true&$orderby=FirstName
-    #   # asc&$skip=0&$top=10', {count:'true', orderby:'FirstName asc', skip:'0', top:'10'}, "Bearer
-    #   # {resp['access_token']}") 
-
-    #   RestClient::Request.execute(method: type.to_sym, url: url, payload:
-    #   header, headers: {content_type: 'text/plain', authorization: "Bearer #{access_token}"})
-    # end # resp =
-      # RestClient::Request.execute(type.to_sym, url, params, "Bearer #{access_token}") response resp end
-end end
+  end 
+end
--- lib/apis/nowcerts.rb
+++ lib/apis/nowcerts.rb
@@ -8,11 +8,8 @@ module Nowcerts
   def self.customers_data
   end
 
-  def self.policies_list(resp)
-  	# (method_type, url, resp)
-  	# p "#{resp}"
-  	# RestClient.request('get', 'https://api.nowcerts.com/api/PolicyDetailList()?$count=true&$orderby=FirstName asc&$skip=0&$top=10', {count:'true', orderby:'FirstName asc', skip:'0', top:'10'}, "Bearer #{resp['access_token']}")
-  	RestClient::Request.execute(method: 'get', url: 'https://api.nowcerts.com/api/PolicyDetailList()?$count=true&$orderby=FirstName asc&$skip=0&$top=10', payload: {count:'true', orderby:'FirstName asc', skip:'0', top:'10'}, headers: {content_type: 'text/plain', authorization: "Bearer #{resp['access_token']}"})
+  def self.policies_list(method_type, url, header, resp)
+    Apis::Responses.request(method_type, url, header, resp)
   end
 end
 # end
